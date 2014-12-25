7a63046d1a3d9573b9370ca7c18c5517c1226d45
Joshua <jdong42@gmail.com>
2014-12-25 15:02:06 -0600

    did i do it

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ int main(int, char**)
 {
 	//TestGame game;
 	ScriptingEngine scripter;
-	Script config = scripter.LoadScript("../scripts/config.lua");
+	Script config = scripter.LoadScript("scripts/config.lua");
 	int *width = (int*)scripter.GetNumber(config, "width");
 	int *height = (int*)scripter.GetNumber(config, "height");
 	Window window(*width, *height, "test");
